2ea98d368eb5a6ef4e676167101bbc7a770b3aec
Ahmed-Lasheen <138633326+Ahmed-Lasheen@users.noreply.github.com>
2024-08-28 23:33:34 +0300

    Add files via upload

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ahmedlasheen/turtle_gp5/src
+# Build directory: /home/ahmedlasheen/turtle_gp5/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("gamepack")
